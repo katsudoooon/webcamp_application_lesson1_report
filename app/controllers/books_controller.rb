1f8c6be5338c0b5f67c088809c4240e792b8ca84
katsudoooon <ec2-user@ip-172-31-61-35.ec2.internal>
2021-10-21 15:33:43 +0000

    [Update]3-9 バリデーション実装エラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,20 +15,36 @@ class BooksController < ApplicationController
   end
 
   def create
-    book =Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book =Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
 
   end
 
   def edit
+    @book = Book.find(params[:id])
+
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    end
 
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to '/books'
+    end
 
   end
 
